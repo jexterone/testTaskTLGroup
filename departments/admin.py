a71cf2fbf54e48a29b0ab5fc262004fbdbbf367d
jexterone <allertartur@mail.ru>
2025-03-23 17:38:36 +0400

    admin

--- departments/admin.py
+++ departments/admin.py
@@ -4,7 +4,11 @@ from .models import Department, Employee
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
     list_display = ('name', 'parent')
+    search_fields = ('name',)
+    list_filter = ('parent',)
 
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'position', 'hire_date', 'salary', 'department')
+    search_fields = ('full_name', 'position')
+    list_filter = ('department', 'hire_date')
